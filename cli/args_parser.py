b68b24bbb526029c33baa35a5c7b2b9b9de67629
Wybu <anhquyqt03@gmail.com>
2025-01-22 04:55:54 +0700

    update args_parser

--- cli/args_parser.py
+++ cli/args_parser.py
@@ -8,24 +8,8 @@ def parse_arguments():
         argparse.Namespace: Parsed arguments.
     """
     parser = argparse.ArgumentParser(description="CLI tool for penetration testing")
-    parser.add_argument(
-        "-s", "--shodan",
-        help="Search for devices using Shodan",
-        action="store_true"
-    )
-    parser.add_argument(
-        "-S", "--scan",
-        help="Scan a network for devices",
-        action="store_true"
-    )
-    parser.add_argument(
-        "-m", "--msf",
-        help="Run an exploit using Metasploit",
-        action="store_true"
-    )
-    parser.add_argument(
-        "-c", "--connect",
-        help="Connect to a remote server",
-        action="store_true"
-    )
+    parser.add_argument("-s", "--shodan", help="Search for devices using Shodan", action="store_true")
+    parser.add_argument("-S", "--scan", help="Scan a network for devices", action="store_true")
+    parser.add_argument("-m", "--msf", help="Run an exploit using Metasploit", action="store_true")
+    parser.add_argument("-c", "--connect", help="Connect to a remote server", action="store_true")
     return parser.parse_args()
---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from modules.shodan_search import shodan_search
+from modules.scanner import scan
+from modules.msf import run_exploit
+from modules.socket_connection import create_connection
+from cli.args_parser import parse_arguments  
+
+def main():
+    args = parse_arguments()  
+
+    if args.shodan:
+        shodan_search()
+    elif args.scan:
+        scan()
+    elif args.msf:
+        run_exploit()
+    elif args.connect:
+        create_connection()
+    else:
+        print("No arguments provided")
+
+if __name__ == "__main__":
+    main()
